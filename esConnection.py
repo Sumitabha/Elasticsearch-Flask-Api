cf6ab62baca2723cc7ad6fdcae79199408753170
Sumitabha Guha <sumitabha.guha@gmail.com>
2023-10-17 17:30:17 +0200

    Added files required for deployment on heroku

--- esConnection.py
+++ esConnection.py
@@ -1,3 +1,3 @@
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch('http://localhost:9200')
+es = Elasticsearch('https://39p01rcxin:i1cfmh6d7z@es-demo-8206114611.us-east-1.bonsaisearch.net:443')
